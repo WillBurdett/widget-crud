9c59f7b4c6bd239114e76f656531a78caa20b036
WillBurdett <williamburdett1995@gmail.com>
2022-09-12 14:30:21 +0100

    addWidget service layer unit test added for happy path

--- src/test/java/com/widgetmicroservice/widgetcrud/unittests/services/WidgetServiceTest.java
+++ src/test/java/com/widgetmicroservice/widgetcrud/unittests/services/WidgetServiceTest.java
@@ -2,7 +2,9 @@ package com.widgetmicroservice.widgetcrud.unittests.services;
 
 import com.widgetmicroservice.widgetcrud.enums.Gender;
 import com.widgetmicroservice.widgetcrud.exceptions.NoWidgetsFound;
+import com.widgetmicroservice.widgetcrud.exceptions.WidgetNotFound;
 import com.widgetmicroservice.widgetcrud.models.Widget;
+import com.widgetmicroservice.widgetcrud.models.WidgetReqBody;
 import com.widgetmicroservice.widgetcrud.repositories.WidgetRepo;
 import com.widgetmicroservice.widgetcrud.services.WidgetService;
 import org.junit.Test;
@@ -19,11 +21,9 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.mockito.Mockito.*;
 
-
 @RunWith(SpringRunner.class)
 @WebMvcTest(WidgetService.class)
 public class WidgetServiceTest {
-
 
     @Autowired
     WidgetService undertest;
@@ -56,7 +56,7 @@ public class WidgetServiceTest {
     }
 
     @Test
-    public void getWidgetById() {
+    public void getWidgetById_HappyPath() {
         // given
         Widget expected = new Widget(1L, "Bob", "Smith", 20, Gender.MALE, 150.0, 80.0);
         when(widgetRepo.findById(expected.getId())).thenReturn(Optional.of(expected));
@@ -68,7 +68,35 @@ public class WidgetServiceTest {
     }
 
     @Test
-    public void addWidget() {
+    public void getWidgetById_ThrowsExceptionWhenWidgetNotFound() {
+        // given
+        Optional <Widget> expected = Optional.empty();
+        when(widgetRepo.findById(1L)).thenReturn(expected);
+        // when
+        assertThatThrownBy(() -> {
+            undertest.getWidgetById(1L);
+            // then
+        }).isInstanceOf(WidgetNotFound.class)
+                .hasMessage("widget with id 1 not found");
+        verify(widgetRepo, times(1)).findById(1L);
+    }
+
+    @Test
+    public void addWidget_HappyPath() {
+        // given
+        WidgetReqBody widgetReqBody = new WidgetReqBody("Bob", "Smith", 20, Gender.MALE, 150.0, 80.0);
+        // when
+        undertest.addWidget(widgetReqBody);
+        // then
+        verify(widgetRepo, times(1)).
+                save(new Widget(
+                        null,
+                        widgetReqBody.getFirstName(),
+                        widgetReqBody.getLastName(),
+                        widgetReqBody.getAge(),
+                        widgetReqBody.getGender(),
+                        widgetReqBody.getHeight(),
+                        widgetReqBody.getWeight()));
     }
 
     @Test
